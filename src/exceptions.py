a9c79d105c00016a7e13f349dfd44e4c2cb84d5c
philip.mohsen <philip.mohsen@gmail.com>
2025-02-19 18:08:16 +0200

    Better error_code

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,8 +1,8 @@
 from typing import Tuple
 
 class InvalidGroupMemberTypeError(Exception):
-    error_code = "INVALID_MEMBER_TYPE"
-    
+    error_code = "INVALID_GROUP_MEMBER_TYPE"
+
     def __init__(self, member: object, allowed_types: Tuple[type]) -> None:
         self.member = member
         self.allowed_types = allowed_types
