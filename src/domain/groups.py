5d9d369134b36d5de80a725b93ba94bf3415dd36
philip.mohsen <philip.mohsen@gmail.com>
2025-02-19 19:31:51 +0200

    Reorganization

---
+++ src/domain/groups.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+from typing import Union
+from .model import Group
+from .model import Technology
+
+class TechnologyGroup(Group):
+    def __init__(self, metadata: Technology) -> None:
+        super().__init__()
+        self.validate_member_type(metadata, (Technology,))
+        self.metadata = metadata
+
+    def add(self, member: Union[TechnologyGroup, Technology]) -> None:
+        self.validate_member_type(member, (TechnologyGroup, Technology))
+        self.members.append(member)
+    
+    def __str__(self, level: int = 0) -> str:
+        indent = "  " * level
+        result = f"{indent}{self.metadata}\n"
+        for member in self.members:
+            if isinstance(member, Technology):
+                result += f"{indent}  {member}\n"
+            else:
+                result += member.__str__(level + 1)
+        return result
--- src/domain/model.py
+++ src/domain/model.py
@@ -1,7 +1,5 @@
-from __future__ import annotations
 from abc import ABC
 from abc import abstractmethod
-from typing import Union
 from typing import Sequence
 from exceptions import InvalidGroupMemberTypeError
 
@@ -35,23 +33,3 @@ class Technology:
     
     def __hash__(self) -> int:
         return hash(self.name)
-
-class TechnologyGroup(Group):
-    def __init__(self, metadata: Technology) -> None:
-        super().__init__()
-        self.validate_member_type(metadata, (Technology,))
-        self.metadata = metadata
-
-    def add(self, member: Union[TechnologyGroup, Technology]) -> None:
-        self.validate_member_type(member, (TechnologyGroup, Technology))
-        self.members.append(member)
-    
-    def __str__(self, level: int = 0) -> str:
-        indent = "  " * level
-        result = f"{indent}{self.metadata}\n"
-        for member in self.members:
-            if isinstance(member, Technology):
-                result += f"{indent}  {member}\n"
-            else:
-                result += member.__str__(level + 1)
-        return result
