45714ff7c9ff2ad3f891c2edd41e765a3b134ecb
philip.mohsen <philip.mohsen@gmail.com>
2025-02-19 18:01:20 +0200

    Added dedicated file for exceptions

---
+++ src/exceptions.py
@@ -0,0 +1,10 @@
+from typing import Tuple
+
+class InvalidGroupMemberTypeError(Exception):
+    error_code = "INVALID_MEMBER_TYPE"
+    
+    def __init__(self, member: object, allowed_types: Tuple[type]) -> None:
+        self.member = member
+        self.allowed_types = allowed_types
+        self.message = f"Cannot add member of type {type(member).__name__} to the group. Allowed types: {', '.join(t.__name__ for t in allowed_types)}"
+        super().__init__(self.message)
