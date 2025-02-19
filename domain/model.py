9d6f68e323312aa34f1cacd155d766f94ef7fd4c
philip.mohsen <philip.mohsen@gmail.com>
2025-02-19 17:47:24 +0200

    Initial commit

---
+++ domain/model.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+from abc import ABC
+from abc import abstractmethod
+from typing import Union
+
+class Group(ABC):
+    def __init__(self, name: str, members: list = None) -> None:
+        self.name = name
+        self.members = members or []
+
+    @abstractmethod
+    def add(self, member: object) -> None:
+        pass
+
+class Technology:
+    def __init__(self, name: str) -> None:
+        self.name = name
+    
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Technology):
+            return False
+        return self.name == other.name
+    
+    def __hash__(self) -> int:
+        return hash(self.name)
+    
+class TechnologyGroup(Group):
+    def add(self, member: Union[TechnologyGroup, Technology]) -> None:
+        self.members.append(member)
