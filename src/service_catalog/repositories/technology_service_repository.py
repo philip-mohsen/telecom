4a1282b0c3dc58ff0aee78d6aa82a2c3b93775a5
philip.mohsen <philip.mohsen@gmail.com>
2025-02-22 19:26:36 +0200

    Repository for technology service

---
+++ src/service_catalog/repositories/technology_service_repository.py
@@ -0,0 +1,30 @@
+from src.shared.repositories.base import Repository
+from src.service_catalog.domain.model import TechnologService
+from src.persistance.json_db import JSONDatabase
+from src.persistance.json_mappers.technolog_service_mapper import TechnologyServiceMapper
+from .technology_repository import TechnologyRepository
+
+class TechnologyServiceRepository(Repository[TechnologService]):
+    def __init__(self, json_db: JSONDatabase):
+        self.json_db = json_db
+        self.data = self.json_db.load_data("technology_services")
+        self.technology_repository = TechnologyRepository(json_db)
+
+    def add(self, technology_service: TechnologService) -> None:
+        self.data[technology_service.uuid] = TechnologyServiceMapper.to_dict(technology_service)
+        self.json_db.save_data("technology_services", self.data)
+
+    def get_by_uuid(self, uuid: str) -> TechnologService:
+        data = self.data.get(uuid)
+        if data is None:
+            raise KeyError(f"TechnologyService with uuid {uuid} not found.")
+        return TechnologyServiceMapper.from_dict(data, self.technology_repository)
+
+    def update(self, technology_service: TechnologService) -> None:
+        pass
+
+    def delete(self, uuid: str) -> None:
+        pass
+
+    def get_all(self) -> list[TechnologService]:
+        pass
