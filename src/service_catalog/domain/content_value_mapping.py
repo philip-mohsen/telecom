044062e08d0fb89e855429f8fec78a74b2992221
philip.mohsen <philip.mohsen@gmail.com>
2025-02-25 21:50:29 +0200

    Channel instead of TVPackage

--- src/service_catalog/domain/content_value_mapping.py
+++ src/service_catalog/domain/content_value_mapping.py
@@ -6,5 +6,5 @@ from src.service_catalog.domain.enums import ContentValueType
 CONTENT_VALUE_MAPPING = {
     ContentValueType.MINUTE: MinuteValue,
     ContentValueType.DATA_LIMIT: DataLimitValue,
-    ContentValueType.TV_PACKAGE: ChannelValue,
+    ContentValueType.CHANNEL: ChannelValue,
 }
