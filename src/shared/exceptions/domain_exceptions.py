6b0ea10310971c17248994bf951dfb387e1cd4fa
philip.mohsen <philip.mohsen@gmail.com>
2025-02-28 16:40:49 +0200

    Refactor with base class

--- src/shared/exceptions/domain_exceptions.py
+++ src/shared/exceptions/domain_exceptions.py
@@ -1,16 +1,13 @@
-class DomainException(Exception):
-    def __init__(self, error_code: str, message: str) -> None:
-        super().__init__(message)
-        self.error_code = error_code
+from src.shared.exceptions.base import BaseException
 
-class InvalidNodeEntityTypeError(DomainException):
+class InvalidNodeEntityTypeError(BaseException):
     def __init__(self, message: str) -> None:
         super().__init__(error_code="INVALID_NODE_ENTITY_TYPE", message=message)
 
-class InvalidRootEntityTypeError(DomainException):
+class InvalidRootEntityTypeError(BaseException):
     def __init__(self, message: str) -> None:
         super().__init__(error_code="INVALID_ROOT_ENTITY_TYPE", message=message)
 
-class InvalidParentEntityTypeError(DomainException):
+class InvalidParentEntityTypeError(BaseException):
     def __init__(self, message: str) -> None:
         super().__init__(error_code="INVALID_PARENT_ENTITY_TYPE", message=message)
