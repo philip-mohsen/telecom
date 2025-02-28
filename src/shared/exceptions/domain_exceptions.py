b7d5907030db61d45d125210b8193ef62474867d
philip.mohsen <philip.mohsen@gmail.com>
2025-02-28 12:37:02 +0200

    Centralize exception handling

---
+++ src/shared/exceptions/domain_exceptions.py
@@ -0,0 +1,12 @@
+class DomainException(Exception):
+    def __init__(self, error_code: str, message: str) -> None:
+        super().__init__(message)
+        self.error_code = error_code
+
+class InvalidNodeEntityTypeError(DomainException):
+    def __init__(self, message: str) -> None:
+        super().__init__(error_code="INVALID_NODE_ENTITY_TYPE", message=message)
+
+class InvalidParentEntityTypeError(DomainException):
+    def __init__(self, message: str) -> None:
+        super().__init__(error_code="INVALID_PARENT_ENTITY_TYPE", message=message)
