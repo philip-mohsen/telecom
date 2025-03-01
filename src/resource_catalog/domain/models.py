1d5634fae549488cacc79819ce03d366d60c1db3
philip.mohsen <philip.mohsen@gmail.com>
2025-03-01 19:16:12 +0200

    Delete all bounded domain contexts

--- src/resource_catalog/domain/models.py
+++ src/resource_catalog/domain/models.py
@@ -1,28 +1,18 @@
-from typing import Optional
 from src.shared.domain.models import Entity
-from src.shared.domain.models import EntityComposite
 from src.shared.domain.models import Category
-from src.shared.domain.validation.entity_composite_validations import EntityCompositeValidator
+from src.shared.domain.validation.category_validations import CategoryValidator
 
-class Technology(Entity):
-    def __init__(self, uuid: str, name: str, abbreviation: Optional[str] = None) -> None:
+class ResourceSpecification(Entity):
+    def __init__(self, uuid: str, name: str) -> None:
         super().__init__(uuid=uuid)
         self.name = name
-        self.abbreviation = abbreviation
 
     def __str__(self) -> str:
-        if self.abbreviation:
-            return (
-                f"{self.__class__.__name__}(name={self.name}, "
-                f"abbreviation={self.abbreviation})"
-            )
         return f"{self.__class__.__name__}(name={self.name})"
 
-class TechnologyTree(EntityComposite[Technology]):
-    def create_entity_composite_validator(self) -> EntityCompositeValidator:
-        validator = EntityCompositeValidator(
-            node_entity_types = (Category, Technology),
-            root_entity_types = (Category,),
-            parent_entity_types = (Category,)
-        )
+class ResourceCategory(Category[ResourceSpecification]):
+    def create_category_validator(self) -> CategoryValidator:
+        validator = CategoryValidator(
+            member_category_types=(ResourceSpecification,)
+            )
         return validator
--- src/service_catalog/domain/enums.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-class ContentValueType(Enum):
-    MINUTE = "MinuteValue"
-    MESSAGE_COUNT = "MessageCountValue"
-    DATA_LIMIT = "DataLimitValue"
-    INTERNET_SPEED = "InternetSpeedValue"
-    CHANNEL = "ChannelValue"
