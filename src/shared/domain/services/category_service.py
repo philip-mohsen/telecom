c9ee1d928acdcdb67c2bd4e2929704e67af54143
philip.mohsen <philip.mohsen@gmail.com>
2025-03-04 21:38:58 +0200

    Add explicit signature for create_category

--- src/shared/domain/services/category_service.py
+++ src/shared/domain/services/category_service.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from abc import ABC, abstractmethod
-from typing import Any
+from typing import Any, Optional, Sequence
 from src.shared.domain.specifications import Specification
 from src.shared.domain.specifications import TypeSpecification
 from src.shared.domain.models import EntityT
@@ -23,16 +23,29 @@ class CategoryService(ABC):
             "members": TypeSpecification((tuple,))
         }
 
-    def create_category(self, **kwargs) -> Category[EntityT]:
-        constructor_specification = self.get_constructor_specification()
-        for key, specification in constructor_specification.items():
-            if not specification(kwargs.get(key)):
-                raise ValueError(f"Invalid value for {key}")
+    def validate_constructor_attribute(self, attribute_name: str, value: Any) -> None:
+        specification = self.get_constructor_specification().get(attribute_name)
+        if not specification(value):
+            raise ValueError(f"Invalid value for {attribute_name}")
 
-        return Category(**kwargs)
+    def create_category(
+            self,
+            uuid: str,
+            name: str,
+            description: Optional[str],
+            members: Sequence[EntityT] = ()
+            ) -> Category[EntityT]:
+
+        self.validate_constructor_attribute("uuid", uuid)
+        self.validate_constructor_attribute("name", name)
+        self.validate_constructor_attribute("description", description)
+        self.validate_constructor_attribute("members", members)
+
+        return Category(uuid=uuid, name=name, description=description, members=members)
 
     def add_member(self, category: Category[EntityT], member: EntityT) -> Category[EntityT]:
         member_specification: Specification[EntityT] = self.get_member_specification()
+
         if not member_specification(member):
             raise ValueError("Invalid member")
 
