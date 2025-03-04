23c5e441d72f4be8b4a1d60bb83bf7d95dac06e5
philip.mohsen <philip.mohsen@gmail.com>
2025-03-04 21:30:15 +0200

    TypeSpecification is handling required

--- src/shared/domain/specifications/__init__.py
+++ src/shared/domain/specifications/__init__.py
@@ -1,9 +1,7 @@
 from .specification import Specification
-from .required_specification import RequiredSpecification
 from .type_specification import TypeSpecification
 
 __all__ = [
     "Specification",
-    "RequiredSpecification",
     "TypeSpecification"
     ]
--- src/shared/domain/specifications/required_specification.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-from dataclasses import dataclass
-from typing import Any
-from src.shared.domain.specifications.specification import Specification
-
-@dataclass(frozen=True)
-class RequiredSpecification(Specification[Any]):
-    def is_satisfied_by(self, candidate: Any) -> bool:
-        return candidate is not None
