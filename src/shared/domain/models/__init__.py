bc8038f53f4bfce1b5dc900490f8e3a3706019f3
philip.mohsen <philip.mohsen@gmail.com>
2025-03-08 01:23:06 +0200

    Expose more classes

--- src/shared/domain/models/__init__.py
+++ src/shared/domain/models/__init__.py
@@ -1,11 +1,29 @@
 from .entity import Entity
 from .entity import EntityT
 from .value_object import ValueObject
+from .value_object import ValueT
 from .category import Category
+from .characteristic_template import CharacteristicTemplate
+from .characteristic_value_template import CharacteristicValueTemplate
+from .characteristic_value_template import StringCharacteristicValueTemplate
+from .characteristic_value_template import IntegerCharacteristicValueTemplate
+from .characteristic_value_template import FloatCharacteristicValueTemplate
+from .characteristic_value_template import BooleanCharacteristicValueTemplate
+from .characteristic_value_template import DateCharacteristicValueTemplate
+from .characteristic_value_template import DataType
 
 __all__ = [
     "Entity",
     "EntityT",
     "ValueObject",
-    "Category"
+    "ValueT",
+    "Category",
+    "CharacteristicTemplate",
+    "CharacteristicValueTemplate",
+    "StringCharacteristicValueTemplate",
+    "IntegerCharacteristicValueTemplate",
+    "FloatCharacteristicValueTemplate",
+    "BooleanCharacteristicValueTemplate",
+    "DateCharacteristicValueTemplate",
+    "DataType"
     ]
